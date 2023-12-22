60c1e5ad10cb8706bafd39485e0d4b78381372ce
R J Sanjanaa <sanjanaarj2003@gmail.com>
2023-12-22 22:57:47 +0530

    Day 33: Add sky conditions and final commit

--- backend.py
+++ backend.py
@@ -2,22 +2,24 @@ import requests
 
 API_KEY = "7f08eea83d936e300c3ba1dc9c7ffd7a"
 
-def get_data(place, forecast_days=None, kind=None):
-    url=f"http://api.openweathermap.org/data/2.5/forecast?id=3163858&appid={API_KEY}"
-    # url=f"http://api.openweathermap.org/data/2.5/forecast?id={place}&appid={API_KEY}"
+def get_data(place, forecast_days=None):
+    url=f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data['list']
     nr_values = 8* forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == 'Temperature':
-        filtered_data=[dict["main"]["temp"] for dict in filtered_data]
-    if kind == 'Sky':
-        filtered_data=[dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
-    # "http://api.openweathermap.org/data/2.5/forecast?id=3163858&appid=7f08eea83d936e300c3ba1dc9c7ffd7a
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo",forecast_days=3,kind="Temperature"))
+    print(get_data(place="Tokyo",forecast_days=3))
+
+
+
+
+# id=3163858
+
+
+# "http://api.openweathermap.org/data/2.5/forecast?id=3163858&appid=7f08eea83d936e300c3ba1dc9c7ffd7a
 
 # http://api.openweathermap.org/data/2.5/forecast?id=Tokyo&appid=7f08eea83d936e300c3ba1dc9c7ffd7a
--- main.py
+++ main.py
@@ -2,17 +2,40 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
+#Add title, text input, slider, selectbox and subheader
 st.title("Weather Forecast for the Next Days")
 place=st.text_input("Place:")
 days=st.slider("Forecast Days",min_value=1,max_value=5,
                help="Select the number of forecasted days")
 option=st.selectbox("Select data to view",
                 ("Temperature","Sky"))
-option = st.subheader(f"{option} for the next {days} days in {place}")
+st.subheader(f"{option} for the next {days} days in {place}")
 
-data=get_data(place, days, option)
-d,t = get_data(days)
+if place:
+    try:
+        # Get the temperature or sky data
+        filtered_data = get_data(place, days)
 
-figure=px.line(x=d,y=t, labels={"x":"Date","y":"Temperature (C)"})
-st.plotly_chart(figure)
-#RUN "      streamlit run E:\app7-weather-forecast-data-app\main.py     " on terminal
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"]/10 for dict in filtered_data]
+            dates=[dict["dt_txt"] for dict in filtered_data]
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
+        if option == "Sky":
+            images = {"Clear":"images/clear.png","Clouds":"images/cloud.png",
+                      "Rain":"images/rain.png","Snow":"images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            print(sky_conditions)
+            st.image(image_paths,width=115)
+    except KeyError:
+            st.write("That place does not exist.")
+
+
+#RUN
+# streamlit run E:\app7-weather-forecast-data-app\main.py
+# on terminal
+
+#Enter the place name as any country - India/London/Tokyo/Tarino
+# Choose data to view as sky or temperature and observe
